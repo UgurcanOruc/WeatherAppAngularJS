818300fb6cdc52bb374ae7f5623dcee36e3b26d8
UgurcanOruc <ugurcan@abisteknoloji.com.tr>
2022-05-13 10:09:00 +0300

    * Unused code deleted.

--- AngularFiles/controllers.js
+++ AngularFiles/controllers.js
@@ -2,8 +2,6 @@
 
 weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function ($scope, $location, cityService) {
 
-    $scope.city = cityService.city;
-
     $scope.$watch('city', function() {
        cityService.city = $scope.city;
     });
